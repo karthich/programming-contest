0604e0f72963bc7dbc85f69a58ffb9f4f94029ef
Karthic Hariharan <karthich@gmail.com>
2012-08-04 12:22:47 -0700

    organised files
    put all the code in a completed folder. separated java and c++ files

---
+++ Spoj Solutions/Java/Completed/Tables.java
@@ -0,0 +1,39 @@
+package spojStuffAll;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Tables {
+
+	/**
+	 * http://www.spoj.pl/problems/AE1B/
+	 */
+	public static void main(String[] args) {
+		int n,k,s;
+		Scanner in1 = new Scanner(System.in);
+		
+		n = in1.nextInt();
+		k = in1.nextInt();
+		s = in1.nextInt();
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>(n);
+		for(int i=0;i<n;i++)
+		{
+			q.offer(-1*in1.nextInt());
+			
+		}
+				
+		int targetSum = k*s;
+		int count=0;
+		while(targetSum>0)
+		{
+			int ai = -1*q.poll();
+			targetSum-=ai;
+			count++;
+		}
+		System.out.print(count+" ");
+		return;
+
+	}
+
+}
