77786d178dd442ab239457342a9cd85d19d6e292
Karthic Hariharan <karthich@gmail.com>
2012-08-04 20:17:01 -0700

    BankAccounts working but tle
    Finished a working model of bank accounts program. But got tle. probably
    need to change scanner to buffereddatareader?

--- Spoj Solutions/Java/spojStuffAll/BankAccounts.java
+++ Spoj Solutions/Java/spojStuffAll/BankAccounts.java
@@ -1,7 +1,11 @@
 package spojStuffAll;
 
-
+//https://www.spoj.pl/problems/SBANK/
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class BankAccounts {
 
@@ -10,18 +14,46 @@ public class BankAccounts {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = new String("03 10103538 2222 1233 6160 0142");
-		String b = new String("03 10103538 2222 1233 6160 0141");
+		TreeMap<String, Integer> bankAccountDb = new TreeMap<String, Integer>();
+		Scanner sc = new Scanner(System.in);
+		int numTestCases=Integer.parseInt(sc.nextLine());
+		int numberAccounts = 0;
 		
-		PriorityQueue<String> pq = new PriorityQueue<String>();
+		for(int i=0;i<numTestCases;i++)
+		{
+			
+			String tempAcc;
+			numberAccounts = Integer.parseInt(sc.nextLine());
+			for(int j=0;j<numberAccounts;j++)
+			{
+				tempAcc = sc.nextLine().trim();
+				Integer accountCount = bankAccountDb.get(tempAcc);
+				if(accountCount==null)
+				{
+					bankAccountDb.put(tempAcc , 1);
+				}
+				else
+				{
+					bankAccountDb.put(tempAcc, accountCount+1);
+				}
+				accountCount = null;
+			}
+			
+			for(Map.Entry<String, Integer> entry : bankAccountDb.entrySet())
+			{
+				System.out.println(entry.getKey() + " " + entry.getValue());
+			}
+			System.out.println();
+		   sc.nextLine();
+			
+			bankAccountDb.clear();
+		}
 		
-		pq.add(a);
-		pq.add(b);
-		pq.add("30 10103538 2222 1233 6160 0142");
 		
-		System.out.println(pq.poll());
-		System.out.println(pq.poll());
-		System.out.println(pq.poll());
+		
+		
+		
+		
 	}
 
 }
