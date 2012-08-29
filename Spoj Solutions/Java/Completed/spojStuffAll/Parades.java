70f095fcfc0fa4236aaf79f85a53e0d0a9104a69
Karthic Hariharan <karthich@gmail.com>
2012-08-29 00:51:12 -0700

    finished parades and submitted
    dint submit yes instead was sending Yes. :((

---
+++ Spoj Solutions/Java/Completed/spojStuffAll/Parades.java
@@ -0,0 +1,79 @@
+package spojPrograms;
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Parades {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		   int testCaseNum = 0;
+		   String caseList[];
+		   String x = "";
+		try {
+			while((x = br.readLine()).compareTo("0")!=0)
+			   {
+					testCaseNum = Integer.parseInt(x);
+					x = br.readLine();
+					caseList = x.split("[ ]");
+					stpar(caseList,testCaseNum);
+			   }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+		
+		public static void stpar(String[] ipStr,int testCaseNum)
+		{
+						 
+			
+			Stack<Integer> ps = new Stack<Integer>();
+			//ps.ensureCapacity(testCaseNum);
+			boolean state = true;
+			int need =1;
+			
+			for(int i=0;i<ipStr.length;i++)
+			{
+				while(!ps.isEmpty()&&(ps.peek()==need))
+				{
+					need++;
+					ps.pop();
+				}
+				if(Integer.parseInt(ipStr[i])==need)
+				{
+					need++;
+				}
+				else if(!ps.isEmpty() && (ps.peek() <Integer.parseInt(ipStr[i])))
+				{
+					state = false;
+					break;
+				}
+				else
+				{
+					ps.push(Integer.parseInt(ipStr[i]));
+				}
+			}
+						
+			if(state)
+				System.out.println("yes");
+			else
+				System.out.println("no");
+			
+			return;
+			
+		}
+
+	}
+
