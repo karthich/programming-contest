4b842eb48b45a95cf806c960f81020613cd079a1
karthich <karthich@karthich-SVS151190X.(none)>
2012-12-09 19:04:45 -0600

    making the bank account commit

--- spoj/cpp/bankAccounts.cpp
+++ spoj/cpp/bankAccounts.cpp
@@ -1,14 +1,26 @@
 #include<stdio.h>
 #define MAX_ACCOUNTS 100000
 int main() {
-  char in[26];
-  scanf("%c%c %c%c%c%c%c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c"
-    ,&in[0],&in[1]
-    ,&in[2],&in[3],&in[4],&in[5],&in[6],&in[7],&in[8],&in[9]
-    ,&in[10],&in[11],&in[12],&in[13]
-    ,&in[14],&in[15],&in[16],&in[17]
-    ,&in[18],&in[19],&in[20],&in[21]
-    ,&in[22],&in[23],&in[24],&in[25]);
-  printf("%c%c %c%c%c%c%c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c\n",in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7],in[8],in[9],in[10],in[11],in[12],in[13],in[14],in[15],in[16],in[17],in[18],in[19],in[20],in[21],in[22],in[23],in[24],in[25]);
+  char in[MAX_ACCOUNTS][26];
+  int t;
+  scanf("%d",&t);
+  int tcount;
+  char c;
+  while(t--) {
+    scanf("%d%c",&tcount,&c);
+    for( int i =0;i<tcount;i++) {
+      scanf("%c%c %c%c%c%c%c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c"
+       ,&in[i][0],&in[i][1]
+       ,&in[i][2],&in[i][3],&in[i][4],&in[i][5]
+       ,&in[i][6],&in[i][7],&in[i][8],&in[i][9]
+       ,&in[i][10],&in[i][11],&in[i][12],&in[i][13]
+       ,&in[i][14],&in[i][15],&in[i][16],&in[i][17]
+       ,&in[i][18],&in[i][19],&in[i][20],&in[i][21]
+       ,&in[i][22],&in[i][23],&in[i][24],&in[i][25]);
+    }
+    for(int i=0;i<tcount;i++) {
+      printf("%c%c %c%c%c%c%c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c %c%c%c%c\n",in[i][0],in[i][1],in[i][2],in[i][3],in[i][4],in[i][5],in[i][6],in[i][7],in[i][8],in[i][9],in[i][10],in[i][11],in[i][12],in[i][13],in[i][14],in[i][15],in[i][16],in[i][17],in[i][18],in[i][19],in[i][20],in[i][21],in[i][22],in[i][23],in[i][24],in[i][25]);
+    }
+  }  
   return 0;
 }
