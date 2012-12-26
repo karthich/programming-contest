0575ce6114e0abc6099382e1959a3e4ba698bd24
Karthic Hariharan <karthich@gmail.com>
2012-12-25 20:50:49 -0600

    attempting common permutations

--- spoj/cpp/cmprt.cpp
+++ spoj/cpp/cmprt.cpp
@@ -1,39 +1,60 @@
-#include "stdafx.h"
+#include<string>
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
 using namespace std;
+
+string merge(string first,string second) {
+	int i=0,j=0;
+	string ans = "";
+	while(i<first.length() && j<second.length()) {
+		if(first[i] == second[j]) {
+			ans = ans + first[i];
+			i++;
+			j++;
+
+		} else {
+			if(first[i] < second[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+	}
+	return ans;
+}
+string cmprt(string first,string second) {
+	sort(first.begin(),first.end());
+	sort(second.begin(),second.end());
+	if(first.size() < second.size()) {
+		string temp = first;
+		first = second;
+		second = temp;
+	}
+	return merge(first,second);
+}
 int main() {
-	map<char,char> first;
-	map<char,char> second;
 	char c;
 	char dummy;
 	int num = 0;
+	string x,first,second;
+
 	while(scanf("%c",&c)!=EOF) {
-		if(c=='\n') {
+		if(c == '\n') {
 			num = (num+1)%2;
-			if(num==0 && !first.empty() && !second.empty()) {
-				map<char,char>::iterator it1=first.begin(),it2=second.begin();
-				while(it1!=first.end() && it2!=second.end()) {
-					if(it1->first == it2->first) {
-						printf("%c",it1->first);
-						it1++;
-						it2++;
-					} else if (it1->first > it2->first) {
-						it2++;
-					} else {
-						it1++;
-					}
-				}
-				printf("\n");
+			if(num == 0) {
+				cout<<cmprt(first,second)<<endl;
 				first.clear();
 				second.clear();
 			}
 		} else {
-			if(num==0) {
-				first.insert(pair<char,char>(c,c));
+			if(num == 0) {
+				first += c;
 			} else {
-				second.insert(pair<char,char>(c,c));
+				second += c;
 			}
 		}
-		
 	}
 	return 0;
 	
